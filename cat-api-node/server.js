523edc0599a8f76b436c4a806d145e480483277b
aerrata <53637476+aerrata@users.noreply.github.com>
2025-05-25 16:58:32 +0800

    🎨 style: reformat code

--- cat-api-node/server.js
+++ cat-api-node/server.js
@@ -1,9 +1,9 @@
-import app from './app.js';
+import app from './app.js'
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
+  console.log(`App running on port ${port}`)
+})
 
-export default server;
+export default server
